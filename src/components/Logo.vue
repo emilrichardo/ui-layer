77bf7a8e61549ce0fcc2bdbb208a1e7593ee2769
emilrichardo <emilrichardo@gmail.com>
2023-03-29 18:26:05 -0300

    add components

---
+++ src/components/Logo.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtImg
+    class="w-full"
+    v-if="darkModeUrl || lightModeUrl"
+    :alt="name"
+    :src="isDarkMode && darkModeUrl ? darkModeUrl : lightModeUrl"
+  />
+  <h1 v-else-if="name" class="text-xl">{{ name }}</h1>
+</template>
+<script setup>
+const { name, isDarkMode, darkModeUrl, lightModeUrl } = defineProps([
+  "name",
+  "isDarkMode",
+  "darkModeUrl",
+  "lightModeUrl",
+]);
+</script>
